174268e333614fbf0d1aee2890c69bbed06e5018
kati <katiacuevasj@gmail.com>
2021-08-19 12:50:58 -0500

    Updates

---
+++ data.js
@@ -0,0 +1,9 @@
+//Aquí contenemos toda la funcionalidad a obtener,procesar y manipular datos
+// estas funciones son de ejemplo
+export const example = () => {
+  return 'example';
+};
+
+export const anotherExample = () => {
+  return 'OMG';
+};
